097e473ba6d85d3800b3bf469b1e7cf9b7ab93c2
Fix <eric@gevanni.com>
2025-12-15 02:01:44 +0200

    Incremental update

--- electron-main.js
+++ electron-main.js
@@ -8,7 +8,7 @@ const { ipcMain, session, clipboard } = require('electron');
 
 const trace = console.log;
 
-// Test 4
+// Test 5
 
 let initial_website_url = 'https://www.plazmaburst.net';
 if ( process.argv.indexOf( '--PB3_FORCE_LOCALHOST' ) !== -1 )
